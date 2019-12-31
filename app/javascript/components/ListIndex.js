430977e1df565d6dfaae1fbbce8a297490f60e7e
Tomashiwa <wongqinliang@gmail.com>
2019-12-31 12:43:41 +0800

    Base implementation of FlexBox

--- app/javascript/components/ListIndex.js
+++ app/javascript/components/ListIndex.js
@@ -1,5 +1,12 @@
 import React from "react";
 import TaskIndex from "./TaskIndex"
+import { FormHelperText, withStyles } from "@material-ui/core";
+
+// const styles = {
+//     listsSection: {
+//         display: flex
+//     }
+// };
 
 class ListIndex extends React.Component {
     constructor(props) {
@@ -12,13 +19,24 @@ class ListIndex extends React.Component {
         return (
             <div>
                 <h1 align="center">Board</h1>
-                <TaskIndex board_id={0} list_id={1} />
-                <TaskIndex board_id={0} list_id={2} />
-                <TaskIndex board_id={0} list_id={3} />
-                <TaskIndex board_id={0} list_id={4} />
+                <section style={{display: "flex", justifyContent:"space-around", alignItems: "flex-start"}}>
+                    <article style={{flexGrow: 1, flexBasis: 0}} >
+                        <TaskIndex board_id={0} list_id={1} />
+                    </article>
+                    <article style={{flexGrow: 1, flexBasis: 0}}>
+                        <TaskIndex board_id={0} list_id={2} />
+                    </article>
+                    <article style={{flexGrow: 1, flexBasis: 0}}>
+                        <TaskIndex board_id={0} list_id={3} />
+                    </article>
+                    <article style={{flexGrow: 1, flexBasis: 0}}>
+                        <TaskIndex board_id={0} list_id={4} />
+                    </article>
+                </section>
             </div>
         );
     }
 }
 
 export default ListIndex;
+// export default withStyles(styles)(ListIndex);
