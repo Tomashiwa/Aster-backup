daf65cb7bf02107751b48dde6266199cf126fc71
Tomashiwa <wongqinliang@gmail.com>
2020-01-09 23:15:50 +0800

    Setup dropdown select for ParticipantList

--- app/javascript/components/ParticipantList.js
+++ app/javascript/components/ParticipantList.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { List, ListItem, IconButton, Typography } from "@material-ui/core";
+import { List, ListItem, IconButton, Typography, Select, FormControl, MenuItem, InputLabel } from "@material-ui/core";
 
 import AddIcon from '@material-ui/icons/Add';
+import DoneIcon from '@material-ui/icons/Done';
+import CloseIcon from '@material-ui/icons/Close';
 
 import UserInfo from "./UserInfo";
 
@@ -10,9 +12,22 @@ import "./styles/ParticipantList.css"
 class ParticipantList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isAdding: false,
+            newPartiId: 1
+        }
     }
 
-    onClick = () => {
+    handleClick = () => {
+        this.setState({isAdding: true});
+    }
+
+    handleClose = () => {
+        this.setState({isAdding: false});
+    }
+
+    handleAdd = () => {
+
     }
 
     render() {
@@ -30,11 +45,41 @@ class ParticipantList extends React.Component {
                           </ListItem>
                       ))
                   }
-                  <ListItem id="addpart_item" key={this.props.users.length + 1} disableGutters={true}>
-                    <IconButton id="addpart_icon" onClick={this.onClick}>
-                        <AddIcon />
-                    </IconButton>
-                  </ListItem>
+
+                  {
+                      this.state.isAdding
+                        ? <ListItem id="field_part" key={this.props.users.length + 1} disableGutters={true}>
+                            <FormControl style={{display: "flex"}}>
+                                <InputLabel shrink id="inputLabel_add_part">New participant</InputLabel>
+                                <Select
+                                    labelId="select_new_partId"
+                                    id="select_new_part"
+                                    value={this.state.newPartiId}
+                                    onChange={this.handleChange}
+                                >
+                                    {
+                                        this.props.users.map(user =>
+                                            <MenuItem key={user.id} value={user.id}>{user.attributes.name}</MenuItem>
+                                        )
+                                    }
+                                </Select>
+                            </FormControl>
+                            <IconButton size="small">
+                                <DoneIcon />
+                            </IconButton>
+                            <IconButton size="small" onClick={this.handleClose}>
+                                <CloseIcon />
+                            </IconButton>
+                        </ListItem> 
+
+                        : <ListItem id="addpart_item" key={this.props.users.length + 1} disableGutters={true}>
+                            <IconButton id="addpart_icon" onClick={this.handleClick}>
+                                <AddIcon />
+                            </IconButton>
+                        </ListItem>
+                  }
+
+                  
                 </List>
 
             </div>
